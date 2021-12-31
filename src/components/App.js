782272d916935fade990e45ee0e75001a94b2017
chethanck <chethanckm66@gmail.com>
2021-12-31 14:14:02 +0530

    React router dom Routes and Links

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import {BrowserRouter, Route, Routes, Link} from "react-router-dom"
 
 const PageOne = () => {
     return (
-        <div>PageOne</div>
+        <div>
+        PageOne
+        {/* <a href="/pagetwo">Navigate to Page2</a> -- Bad Approach Because it'll rerender the whole index.html*/}
+        <Link to="/pagetwo">Navigate to Page2</Link>
+        </div>
     )
 }
 
 const PageTwo = () => {
     return (
-        <div>PageTwo</div>
+        <div>
+        PageTwo
+        {/* <a href="/">Navigate to Page1</a> */}
+        <Link to="/">Navigate to Page1</Link>
+        </div>
     )
 }
 
@@ -19,7 +27,7 @@ const App = () => {
             <BrowserRouter>
                 <div>
                 <Routes>
-                    <Route path="/" exact element={PageOne()}/>
+                    <Route path="/" element={PageOne()}/>
                     <Route path="/pagetwo" element={PageTwo()} />
                 </Routes>
                 </div>
