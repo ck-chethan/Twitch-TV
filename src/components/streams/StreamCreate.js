178650803ab8b66a143d5f6d8cc2be84e2b75318
chethanck <chethanckm66@gmail.com>
2022-01-02 16:04:30 +0530

    Redux Form complete overview

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -13,14 +13,14 @@ class StreamCreate extends React.Component {
         }
     }
 
-    renderInput = ({input, label, meta}) => {//converting it to render function because 
-                                             //we don't know what the value of this in many instances.
-        console.log(meta);
+    renderInput = ({input, label, meta}) => {
+        //converting it to render function because we don't know what the value of this in many instances.
         //return (<input onChange={formProps.input.onChange} value={formProps.input.val}/>);
         //This can be written using shortcut
         //return (<input {...formProps.input} />);
+        const className = `field ${meta.error && meta.touched ? 'error' : ''}`
         return (
-            <div className="field">
+            <div className={className}>
                 <label>{label}</label>
                 <input {...input} autoComplete="off"/>
                 {this.renderError(meta)}
@@ -34,7 +34,7 @@ class StreamCreate extends React.Component {
 
     render(){
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter a Title"/>
                 <Field name="description" component={this.renderInput} label="Enter a Description"/>
                 <button className="ui button primary">Submit</button>
