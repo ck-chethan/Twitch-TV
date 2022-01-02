3e0d4eba75f7c2dbbbf63a96b19d97352deb5560
chethanck <chethanckm66@gmail.com>
2022-01-02 14:55:11 +0530

    Redux Form input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,15 +3,23 @@ import {Field, reduxForm} from "redux-form";
 
 class StreamCreate extends React.Component {
 
-    renderInput(formProps){
-        return (<input onChange={formProps.input.onChange} value={formProps.input.value}/>);
+    renderInput({input, label}){
+        //return (<input onChange={formProps.input.onChange} value={formProps.input.val}/>);
+        //This can be written using shortcut
+        //return (<input {...formProps.input} />);
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
     }
 
     render(){
         return (
-            <form action="">
-                <Field name="title" component={this.renderInput}/>
-                <Field name="description" component={this.renderInput}/>
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter a Title"/>
+                <Field name="description" component={this.renderInput} label="Enter a Description"/>
             </form>
         );
     };
